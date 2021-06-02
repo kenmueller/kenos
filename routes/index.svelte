3af26d19ed3d3a5d91b8f327a5d326f5a5e15620
kenmueller <kenmueller0@gmail.com>
2021-06-01 17:03:19 -0700

    Initial commit

---
+++ routes/index.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+	<title>kenOS</title>
+</svelte:head>
+
+<main>
+	<h1>kenOS</h1>
+</main>
+
+<style lang="scss">
+	main {
+		display: flex;
+		height: 100%;
+	}
+
+	h1 {
+		margin: auto;
+	}
+</style>
